d8dfd1eaa358430086ed97dc4738a4fd764b084c
otaviopacheco <aapo5719@gmail.com>
2024-03-16 13:44:24 -0300

    primeira atividade concluida

---
+++ 0readme.md
@@ -0,0 +1,1 @@
+Os exercicios no vscode começam aqui 
---
+++ ex3_e-h.py
@@ -0,0 +1,37 @@
+import math 
+# Exercicio E 
+# a = 10 
+# b = 5
+# c = a + b
+# b = 20 
+# a = 10 
+# print(a,b,c)
+
+# Exercicio F
+# x = 1 
+# y = 2
+# z = y - x
+# print(z)
+# x = 5
+# y = x + z 
+# print(x,y,z)
+
+#  Exercicio G 
+# x = 2 
+# y = x + 1
+# w = y + 1
+# z = "X"
+# print(w,z)
+# x = y^2
+# y = x-w 
+# z = "Y"
+# print(y,z)
+# Exercicio H 
+# x = 8 
+# y = x^2
+# w = y + x 
+# z = "X"
+# x = sqrt(Y)
+# w = x + 2 
+# z = "X"
+# print(y,z)
