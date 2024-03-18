ad4779b73569efd22b700d3da760f0242277bfe3
otaviopacheco <aapo5719@gmail.com>
2024-03-17 21:47:51 -0300

    exercicio 2 feito

---
+++ atv6/exercicios_1-5/ex02.py
@@ -0,0 +1,5 @@
+areaMenor = float(input("insira o valor da base menor: "))
+areaMaior = float(input("insira o valor da base maior: "))
+tAltura = float(input("insira o valor da altura: "))
+area = (( areaMaior+ areaMaior)*tAltura)/2
+print(area)
