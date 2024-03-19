83b9a1eb8b0ad3624e20702391aad6cf739dc0e9
Otavio <aapo5719@gmail.com>
2024-03-19 15:26:32 -0300

    atividade 9 e 10 finalizadas

---
+++ atv6/exercicios_6-10/ex09.py
@@ -0,0 +1,4 @@
+jantaVl = float(input("Insira o valor total da janta"))
+taxaGar = jantaVl * (10 / 100)
+valorTot = jantaVl + taxaGar
+print("total a pagar: " + valorTot)
---
+++ atv6/exercicios_6-10/ex10.py
@@ -0,0 +1,5 @@
+totDias = int(input("Insira o total de dias de viagem: "))
+horasAd = int(input("Insira o total de horas adjacentes de viagem: "))
+totDias = totDias * 24 
+tempoTot = int(totDias) + int(horasAd)
+print(tempoTot)
