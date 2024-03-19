5a7d4abda5268e12dd7ce1b528fcbaff13b9f791
Otavio <aapo5719@gmail.com>
2024-03-19 14:41:52 -0300

    exercicio 6 e 7 terminados

---
+++ atv6/exercicios_6-10/ex06.py
@@ -0,0 +1,5 @@
+contaTotal = float(input("insira o valor total da conta "))
+qntClientes = int(input("Insira a quantidade de clientes "))
+prtPrCl = contaTotal / qntClientes
+print("O valor a ser pago por cada cliente é", prtPrCl)
+
