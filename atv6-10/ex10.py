bd24490ced566d7928705703d2749df551f82c44
otavio <aapo5719@gmail.com>
2024-03-27 16:44:18 -0300

    corrigidos ex09-10

--- atv6-10/ex10.py
+++ atv6-10/ex10.py
@@ -1,10 +1,10 @@
 velocidadePermitida = int(input("insira a velocidade permitida na via: "))
 velocidadeAlcancada = int(input("insira a velocidade alcancada na via: "))
 velocidadeAplicada = velocidadePermitida * (20/100)
-if velocidadePermitida < velocidadeAlcancada+velocidadeAplicada:
-    print("Multa grave")
-    print(velocidadeAlcancada+velocidadeAplicada)
+if velocidadeAlcancada >= velocidadePermitida+velocidadeAplicada:
+    if velocidadePermitida < velocidadeAlcancada+velocidadeAplicada:
+        print("Multa grave")
 else:
-    print(velocidadeAlcancada+velocidadeAplicada)
+    print("a multa seria aplicada caso vc estivesse",velocidadeAlcancada+velocidadeAplicada)
     print("Sem Multa")
     
