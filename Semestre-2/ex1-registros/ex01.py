84feb7029fafd24e503a0a79dde6e470a29e7a8b
Otavio <aapo5719@gmail.com>
2024-10-01 19:42:32 -0300

    att da atv

--- Semestre-2/ex1-registros/ex01.py
+++ Semestre-2/ex1-registros/ex01.py
@@ -1,10 +1,12 @@
 nome = ""
 endereco = ""
 fone = 0
+cep = ''
 agenda = {
     'nome': nome,
     'endereco': endereco,
-    'fone': fone
+    'fone': fone,
+    'cep': cep
 }
 vecAgendas = []
 
@@ -15,5 +17,7 @@ for i in range(0,1,1):
     agenda["endereco"] = endereco
     fone= int(input("Insira o Telefone: "))
     agenda["fone"] = fone
+    cep= str(input("Insira o CEP: "))
+    agenda["cep"] = cep
     vecAgendas.append(agenda)
 print(vecAgendas)
