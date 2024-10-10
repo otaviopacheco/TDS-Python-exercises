1262f1d11d2a418860cfb6d54e65b5c061366fe3
Otavio <aapo5719@gmail.com>
2024-10-10 17:09:39 -0300

    ex registros atualizado

--- Semestre-2/estrutura-e-dados/ex1-registros/ex01.py
+++ Semestre-2/estrutura-e-dados/ex1-registros/ex01.py
@@ -2,15 +2,15 @@ variaveis = []
 for i in range(8):
       variaveis.append(input("Insira o dado:"))
 agenda = {
-    0: variaveis[0],
-    1: variaveis[1],
-    2: variaveis[2],
-    3: variaveis[3]
+    0: variaveis[i],
+    1: variaveis[i],
+    2: variaveis[i],
+    3: variaveis[i]
 }
 vecAgendas = []
 agendaTam = len(agenda)
  
-for j in range(1):
+for j in range(2):
     for i in range(0,1,1):
         variaveis[i]=agenda[j]
         vecAgendas.append(agenda)
