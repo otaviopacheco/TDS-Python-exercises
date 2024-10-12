29c295e60b8f1cbc70f56bf7f4bf69b7b4e34cf7
Otavio <aapo5719@gmail.com>
2024-10-12 00:31:19 -0300

    Animação na imagem da loona adicionada

--- Semestre-2/PHP/teste.php
+++ Semestre-2/PHP/teste.php
@@ -1,23 +1,12 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-    $a = 1;
-    $b = 3;
-    $c = 5;
-    $r1 = ++$b - $a;
-    $r2 = $c-- + $a
-    $r3 = --$a + $c++
-    $nome = "Jorgin";
-        echo $nome;
-        echo "A = $a <br> b = $b <br> c = $c <br>"
-        echo "r1=  $r1 <br> r2= $r2 <br> r3 = $r3"
+<?php 
+$a = 1;
+$b = 3;
+$c = 5;
+$r1 = ++$b - $a;
+$r2 = $c-- + $a
+$r3 = --$a + $c++
+$nome = "Jorgin";
+echo $nome;
+echo "A = $a <br> b = $b <br> c = $c <br>";
+echo "r1=  $r1 <br> r2= $r2 <br> r3 = $r3";
 ?>
-</body>
-</html>
