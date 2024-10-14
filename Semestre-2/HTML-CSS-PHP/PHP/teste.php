64a37bc23bc978659a17d25ebec91d5dc0772746
Otavio <aapo5719@gmail.com>
2024-10-14 14:36:30 -0300

    repositorio atualizado

--- Semestre-2/HTML-CSS-PHP/PHP/teste.php
+++ Semestre-2/HTML-CSS-PHP/PHP/teste.php
@@ -3,10 +3,11 @@ $a = 1;
 $b = 3;
 $c = 5;
 $r1 = ++$b - $a;
-$r2 = $c-- + $a
-$r3 = --$a + $c++
+$r2 = $c-- + $a;
+$r3 = --$a + $c++;
 $nome = "Jorgin";
 echo $nome;
+echo "<br>";
 echo "A = $a <br> b = $b <br> c = $c <br>";
 echo "r1=  $r1 <br> r2= $r2 <br> r3 = $r3";
 ?>
