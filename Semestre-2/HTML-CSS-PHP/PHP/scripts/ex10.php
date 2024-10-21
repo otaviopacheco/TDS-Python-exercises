6e1a5233079e87003a27e69532710025fe31550d
otavio <aapo5719@gmail.com>
2024-10-21 15:08:27 -0300

    atv10 finalizada

--- Semestre-2/HTML-CSS-PHP/PHP/scripts/ex10.php
+++ Semestre-2/HTML-CSS-PHP/PHP/scripts/ex10.php
@@ -1,11 +1,9 @@
 <?php
-$possivelMes = [1,2,3,4,5,6,7,8,9,10,11,12];
-$mesInsirido = $_POST["mesInsirido"];
-for ($i = 0; $i <count($possivelMes); $i++) {
-    if ($possivelMes[$i] == $mesInsirido and $mesInsirido ==1){
-        echo "é Janeiro";
+$possivelMes = [ "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro","dezembro"];
+$mesInserido = $_POST["mesInserido"];
+    if($mesInserido > 0 and $mesInserido <= 12){
+        echo"o mes é ".$possivelMes[$mesInserido-1];
+
     }
-
-}
-
+    
 ?>
