c884bfb9bf1b476b06e5ad9f855793f2fecec3a2
Otavio <aapo5719@gmail.com>
2024-10-21 01:18:03 -0300

    atv 8 corrijida

--- Semestre-2/HTML-CSS-PHP/PHP/scripts/ex08.php
+++ Semestre-2/HTML-CSS-PHP/PHP/scripts/ex08.php
@@ -4,7 +4,7 @@ $resp = 0;
 // $resp = ($notas[0] + $notas[1] + $notas[2])/3;
 // print_r($notas)
 for($i = 0; $i < count($notas); $i++){
-    $resp += ($notas[$i])/2;
+    $resp += (($notas[$i])/3);
 }
 echo $resp;
 ?>
