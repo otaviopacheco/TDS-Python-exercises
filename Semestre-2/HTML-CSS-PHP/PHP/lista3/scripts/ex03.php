3eb1ca0214d63f3c3c0fb483e98e422663792074
Otavio <aapo5719@gmail.com>
2024-11-05 09:53:17 -0300

    carrosel secreto

--- Semestre-2/HTML-CSS-PHP/PHP/lista3/scripts/ex03.php
+++ Semestre-2/HTML-CSS-PHP/PHP/lista3/scripts/ex03.php
@@ -5,15 +5,15 @@ $number = $_POST["number"];
 function compararMenor($number){
     for($i = 0; $i < count($number); $i++){
         if ($number[$i] > $number[2] and $number[$i]> $number[1]){
-            echo "o menor numero é $number[$i]";
+            echo "o menor numero é $number[$i] caso1";
             $menor = $number[$i];
             return $menor;
         }else if ($number[$i] < $number[2] and $number[$i] <  $number[0]){
-            echo "o menor numero é $number[$i]";
+            echo "o menor numero é $number[$i] caso2";
             $menor = $number[$i];
             return $menor;
         }else if ($number[$i] < $number[0] and $number[$i]< $number[1]){
-            echo "o menor numero é $number[$i]";
+            echo "o menor numero é $number[$i] caso3";
             $menor = $number[$i];
             return $menor;
         }
---
+++ Semestre-2/HTML-CSS-PHP/PHP/lista3/scripts/ex04.php
@@ -0,0 +1,32 @@
+<?php 
+
+$vecQtd = [];
+$lancamentos = $_POST["rodadas"];
+function qtdlancamentos($lancamentos){
+    $vecDados = [1,2,3,4,5,6];
+    while ($lancamentos > 0){
+        $random = rand(1,6);
+
+
+        for ($i = 0; $i <= $vecDados; $i++){
+
+
+            if ($random == $i-1){
+
+                if ($random == 0){
+                    
+
+                }
+
+
+            }
+
+
+            
+        }
+
+    }
+
+}
+
+?>
