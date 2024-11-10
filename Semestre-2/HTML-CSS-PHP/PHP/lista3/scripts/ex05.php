279f2bd2665db74a1626be8d20f13121c8a42a1a
Otavio <aapo5719@gmail.com>
2024-11-09 21:51:51 -0300

    atv 5 feita

--- Semestre-2/HTML-CSS-PHP/PHP/lista3/scripts/ex05.php
+++ Semestre-2/HTML-CSS-PHP/PHP/lista3/scripts/ex05.php
@@ -1,6 +1,38 @@
 <?php 
-$count = $_POST["fn"]; 
-$count= (int) $count+2;
-if ($count < 0){
-    echo "seu numero foi convertido para inteiro mas ainda é negativo então inicie novamente";
+$fn = $_POST["fn"]; 
+
+function intDetector($fn){
+    
+    if($fn > 0){
+        $fn = (int) $fn;
+        return  true;        
+
+    } else {
+        return false;
+    }
 }
+$fnType = intDetector($fn);
+
+
+
+function Enesimo($fnType,$fn){
+    if  ($fnType == false){
+        echo "Impossivel prosseguir";
+    }elseif ($fnType == true){
+        
+        
+        $termos = [0,1];
+        
+        for ($i = 2; $i < $fn; $i++){ 
+            $termos[$i] = $termos[$i-2] + $termos[$i-1];
+
+        }
+        print_r($termos);
+    }
+
+}
+
+$resp = Enesimo($fnType, $fn);
+echo "$resp<br>";
+
+?>
