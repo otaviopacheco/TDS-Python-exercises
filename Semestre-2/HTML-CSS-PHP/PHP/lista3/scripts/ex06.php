0d429ba40dec282ed87ccbe978ae753fc7f616e1
Otavio <aapo5719@gmail.com>
2024-11-09 22:20:10 -0300

    atv atualizada

--- Semestre-2/HTML-CSS-PHP/PHP/lista3/scripts/ex06.php
+++ Semestre-2/HTML-CSS-PHP/PHP/lista3/scripts/ex06.php
@@ -1,4 +1,22 @@
 <?php
+$numero = $_POST["numero"];
+function impar($numero){
+    if($numero == 2 or $numero % 2!= 0){
+        return true;
+    }
+    return false;
+}
+$eImpar = impar($numero);
+function primo($eImpar,$numero){
+    if ($eImpar == true){
+        return 'rolou';
+
+    }else{
+        return "va pa pqp";
+    }
 
 
+};
+$Eprimo = primo($eImpar,$numero);
+echo $Eprimo;
 ?>
