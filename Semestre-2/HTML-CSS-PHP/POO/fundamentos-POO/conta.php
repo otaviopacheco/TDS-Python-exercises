922207d9bd70712082cdf9774763c39c3c399965
Otavio <aapo5719@gmail.com>
2024-11-21 13:09:35 -0300

    poo organizado

--- Semestre-2/HTML-CSS-PHP/POO/fundamentos-POO/conta.php
+++ Semestre-2/HTML-CSS-PHP/POO/fundamentos-POO/conta.php
@@ -4,10 +4,12 @@ $valorPix = $_POST["valor"];
 class Conta{
     private $numero;
     private $saldo;
+
     function __construct($numero,$saldo){
         $this->numero = $numero;
         $this->saldo = $saldo;
     }
+
     function depositar($valor){
         $this->saldo += $valor;
     }
@@ -18,15 +20,19 @@ class Conta{
     function getNumero(){
         return $this->numero;
     }
+
     function getSaldo(){
         return $this->saldo;
     }
+
     function setSaldo($saldo){
         $this->saldo = $saldo;
     }
 }
 $minhaConta = new Conta($contaNome, $valorPix);
 
+echo $minhaConta-> getNumero()."<br>";
+echo $minhaConta-> getSaldo();
 
 
 ?>  
