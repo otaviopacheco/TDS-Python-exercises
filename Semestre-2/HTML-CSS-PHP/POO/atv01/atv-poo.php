d2ffb2d66f8961521e31ba459c8cfc3c811a5bcf
otavio <aapo5719@gmail.com>
2024-11-21 16:30:11 -0300

    poo 1

--- Semestre-2/HTML-CSS-PHP/POO/atv01/atv-poo.php
+++ Semestre-2/HTML-CSS-PHP/POO/atv01/atv-poo.php
@@ -1,62 +1,15 @@
 <?php
+require "contaBnco.php"; 
+
 $numeroConta = $_POST["numeroConta"];
 $tipoConta = $_POST["tipoConta"];
 $donoConta = $_POST["donoConta"];
 $saldoConta = $_POST["saldoConta"];
 $statusConta = $_POST["statusConta"];
-
-
-class contaBanco {
-    private $numero;
-    private $tipo;
-    private $dono;
-    private $saldo;
-    private $status;
-
-
-    function _construtor($numero, $tipo, $dono, $saldo, $status){
-        $this->numero = $numero;
-        $this->tipo = $tipo; 
-        $this->dono = $dono;
-        $this->saldo = $saldo;
-        $this->status = $status;
-    }
-
-    function setNumero( $numero){
-        $this->numero = $numero;
-    }
-
-    function setTipo($tipo){
-        $this->tipo = $tipo;
-    }
-
-    function setDono($dono){
-        $this->dono = $dono;
-
-    }
-    function setSaldo($saldo){
-        $this->saldo = $saldo;
-    }
-    function setStatus($status){
-        $this -> status = $status;
-    }
-
-
-
-
-
-
-
-    function getNumero(){
-        return $this->numero;
-    }
-    function getTipo(){
-        return $this->tipo;
-    }
-
-
-}
-$novaConta
-
-
+echo "teste = $numeroConta";
+$novaConta = new contaBanco($numeroConta, $tipoConta, $donoConta, $saldoConta, $statusConta);
+$c = $novaConta->getNumero();
+$t = $novaConta->getTipo();
+echo "Número da Conta: ".$c."<br>";
+echo "tipo da Conta: ".$t."<br>";
 ?>
