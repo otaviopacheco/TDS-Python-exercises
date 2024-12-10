6cfc7b95b73af28771ee09ff562583b70ae7f23b
Otavio <aapo5719@gmail.com>
2024-12-10 01:38:38 -0300

    first commit -m

---
+++ Semestre-2/C++/primeiroPrograma.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+
+
+    int vidas;
+    char letra;
+    double decimal;
+    float decimal2;
+    bool tipo; 
+    string nome; 
+
+
+
+    cout << "Digite o numero de vidas";
+    cin >> vidas;
+    cout << nome << "\n" << vidas << "\n" << letra << "\n" << decimal << "\n" << decimal2 << "\n" << tipo << "\n";
+
+
+    return 0;
+
+
+}
+
+
+
+
